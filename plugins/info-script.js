a812879b509ff98b160bf8b95a30a25d2d876d58
dawawasgg <106122054+dawawasgg@users.noreply.github.com>
2022-09-11 22:15:06 +0800

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -2,24 +2,16 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
+Private 🤡
 
-◪ 📮 *SCRIPT BOT*
-│ *Script :* 
-│ ╰ Ada Di GitHub
-│ *NIH :* 
-│ ╰ Tapi Gw Private
-╰──────────═┅═──────────
-`,`📍 *N o t e :* 
-• Jangan lupa minta izin owner sebelum menggunakan scriptnya kak!
-• Minta Sc Ke Owner Pasti Nnti Ga Di Kasih Soalnya Ownernya Pelit Yahaha!
-
+wa.me/6282127487538`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
     mediaUrl: "https://Instagram.com/ohlxmarket",
     mediaType: "VIDEO",
     description: "https://www.instagram.com/p/CiCQ_SnuPdw/?igshid=YmMyMTA2M2Y=", 
-    title: '𝘼𝙎𝙀𝙋𝘽𝙊𝙏𝙕 MultiDevice',
+    title: '𝗢𝗛𝗟𝗫-𝗠𝗗 MultiDevice',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
