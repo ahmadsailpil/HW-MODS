b4c2b0e70f319ea6f8c29b293c66ae3c8d9ea4c9
dawawasgg <106122054+dawawasgg@users.noreply.github.com>
2022-09-12 09:31:04 +0800

    Update _anticulik.js

--- plugins/_anticulik.js
+++ plugins/_anticulik.js
@@ -5,33 +5,32 @@ handler.all = async function (m, { isBlocked }) {
     if (isBlocked) return
     // ketika ada yang invite/kirim link grup di chat pribadi
     if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('Undangan untuk bergabung') || m.text.startsWith('Invitation to join') || m.text.startsWith('Buka tautan ini')) && !m.isBaileys && !m.isGroup) {
-    let teks = `「 WH-MODS-BOT-V1 」
+    let teks = `「 𝗢𝗛𝗟𝗫-𝗠𝗗 」
 
     ╭━━━━「 SEWA 」
-    ┊⫹⫺ Hemat: 5k/grup (1 minggu)
-    ┊⫹⫺ Normal: 15k/grup (1 bulan)
-    ┊⫹⫺ Standar: 30k/grup (2 bulan)
+    ┊⫹⫺ Hemat: 5k/grup (15 Hari)
+    ┊⫹⫺ Normal: 10k/grup (1 bulan)
+    ┊⫹⫺ Standar: 20k/grup (2 bulan)
     ┊⫹⫺ Pro: 35k/grup (4 bulan)                                                      
-    ┊⫹⫺ Vip: = 65k/grup (12 bulan)
+    ┊⫹⫺ Vip: = 50k/grup (12 bulan)
     ╰═┅═━––––––๑
     
     ╭━━━━「 PREMIUM 」
-    ┊⫹⫺ Hemat: 5k (1 minggu)
-    ┊⫹⫺ Normal: 20k (1 bulan)
-    ┊⫹⫺ Pro: 40k (4 bulan)
-    ┊⫹⫺ Vip: 50k (8 bulan)                                               
-    ┊⫹⫺ Permanent: = 70k (Unlimited)
+    ┊⫹⫺ Hemat: 5k (15 hari)
+    ┊⫹⫺ Normal: 10k (1 bulan)
+    ┊⫹⫺ Pro: 30k (4 bulan)
+    ┊⫹⫺ Vip: 40k (8 bulan)                                               
+    ┊⫹⫺ Permanent: = 60k (Unlimited)
     ╰═┅═━––––––๑
     
     ⫹⫺ PAYMENT:
-    • Pulsa Telkomsel: [082127487538]
-    • Dana: [082127487538]
-    • Gopay: [082127487538]
-    • Ovo: [082127487538]
-    • Link Aja: [082127487538]
+    • Dana
+    • Gopay
+    • Ovo
+    • QRIS
     
     Nomor Owner :
-    wa.me/6282127487538
+    wa.me/6283160327945
     
     ▌│█║▌║▌║║▌║▌║█│▌
     
