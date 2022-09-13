4e281927a9a48ce7c75940647d8164ef4238848d
dawawasgg <106122054+dawawasgg@users.noreply.github.com>
2022-09-14 06:18:44 +0800

    Update _anticulik.js

--- plugins/_anticulik.js
+++ plugins/_anticulik.js
@@ -5,22 +5,21 @@ handler.all = async function (m, { isBlocked }) {
     if (isBlocked) return
     // ketika ada yang invite/kirim link grup di chat pribadi
     if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('Undangan untuk bergabung') || m.text.startsWith('Invitation to join') || m.text.startsWith('Buka tautan ini')) && !m.isBaileys && !m.isGroup) {
-    let teks = `「 𝗢𝗛𝗟𝗫-𝗠𝗗 」
-
+    let teks = `Invite Group
     ╭━━━━「 SEWA 」
     ┊⫹⫺ Hemat: 5k/grup (15 Hari)
     ┊⫹⫺ Normal: 10k/grup (1 bulan)
     ┊⫹⫺ Standar: 20k/grup (2 bulan)
     ┊⫹⫺ Pro: 35k/grup (4 bulan)                                                      
-    ┊⫹⫺ Vip: = 50k/grup (12 bulan)
+    ┊⫹⫺ Vip: = 45k/grup (12 bulan)
     ╰═┅═━––––––๑
     
     ╭━━━━「 PREMIUM 」
     ┊⫹⫺ Hemat: 5k (15 hari)
     ┊⫹⫺ Normal: 10k (1 bulan)
-    ┊⫹⫺ Pro: 30k (4 bulan)
-    ┊⫹⫺ Vip: 40k (8 bulan)                                               
-    ┊⫹⫺ Permanent: = 60k (Unlimited)
+    ┊⫹⫺ Pro: 25k (4 bulan)
+    ┊⫹⫺ Vip: 30k (8 bulan)                                               
+    ┊⫹⫺ Permanent: = 50k (Unlimited)
     ╰═┅═━––––––๑
     
     ⫹⫺ PAYMENT:
@@ -29,16 +28,13 @@ handler.all = async function (m, { isBlocked }) {
     • Ovo
     • QRIS
     
-    Nomor Owner :
-    wa.me/6283160327945
-    
-    ▌│█║▌║▌║║▌║▌║█│▌
-    
-    #BY 𝐑𝐈𝐃𝐇𝐎 𝐆𝐀𝐍𝐙`
+
+Jika berminat hubungi: 'http://wa.me/6283160327945' untuk order:)
+`
     this.reply(m.chat, teks, m)
     const data = global.owner.filter(([id, isCreator]) => id && isCreator)
     this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
     }
 }
 
-export default handler
+module.exports = handler
