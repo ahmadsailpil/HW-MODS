05ef82b604ff127a1084658548a00386dde26978
dawawasgg <106122054+dawawasgg@users.noreply.github.com>
2022-09-13 19:08:08 +0800

    Update owner-backup.js

--- plugins/owner-backup.js
+++ plugins/owner-backup.js
@@ -21,7 +21,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
             })
 conn.reply(m.chat, '*Succes*', m)
 conn.reply('6283160327945' + '@s.whatsapp.net', `*🗓️ Database:* ${date}`, null)
-          conn.sendFile('6283160327945' + '@s.whatsapp.net', fs.readFileSync('./index.js_database.json'), 'index.js_database.json', '', 0, 0, { mimetype: 'application/json', quoted: fdoc})
+          conn.sendFile('6283160327945' + '@s.whatsapp.net', fs.readFileSync('database.json'), 'database.json', '', 0, 0, { mimetype: 'application/json', quoted: fdoc})
  }
  
  handler.help = ['backup']
