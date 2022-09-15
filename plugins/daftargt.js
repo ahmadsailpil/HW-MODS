aeea8dd5ce2babbd1a7723186a029e349620752e
dawawasgg <106122054+dawawasgg@users.noreply.github.com>
2022-09-15 23:13:30 +0800

    Update daftargt.js

--- plugins/daftargt.js
+++ plugins/daftargt.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn }) => {
 	//-----PRICE
 //sewa
-let sh = '6.250'
-let sn = '12.500'
-let ss = '18.750'
-let sp = '25.000'
-let sv = '31.250'
+let sh = '6.200'
+let sn = '12.400'
+let ss = '18.600'
+let sp = '24.800'
+let sv = '31.000'
 //premium
 let ph = '5.800'
 let pn = '11.600'
@@ -13,15 +13,15 @@ let pp = '17.400'
 let pv = '23.200'
 let ppm = '29.000'
 let info = `
-╭━━━━「 *Harga Buy DL* 」
+╭━━━━「 *HARGA BUY DL* 」
 ┊⫹⫺ *1 DL:* _${sh}Rp_
 ┊⫹⫺ *2 DL:* _${sn}Rp_
 ┊⫹⫺ *3 DL:* _${ss}Rp_
-┊⫹⫺ *4 DL:* _${sp}Rp_                                                      
+┊⫹⫺ *4 DL:* _${sp}Rp_                                                  
 ┊⫹⫺ *5 DL:* = _${sv}Rp_
 ╰═┅═━––––––๑
 
-╭━━━━「 *Harga Jual DL* 」
+╭━━━━「 *HARGA SELL DL* 」
 ┊⫹⫺ *1 DL:* _${ph}Rp_
 ┊⫹⫺ *2 DL:* _${pn}Rp_
 ┊⫹⫺ *3 DL:* _${pp}Rp_
@@ -30,12 +30,11 @@ let info = `
 ╰═┅═━––––––๑
 
 *⫹⫺ PAYMENT:*
-• *Dana:* 
-• *Gopay:* 
-• *Ovo:* 
+• *Dana:*
+• *Gopay:*
+• *Ovo:*
 
 –––––– *🐾 Kebijakan* ––––––
-
 🗣️: Scam ga nih kak?
 💬: Enggalah, Owner 100% Tepati janji #STAYHALAL
 
@@ -43,22 +42,22 @@ let info = `
 `
 const sections = [
    {
-	title: `${htjava} HARGA BUY DL ✦-------`,
+	title: `${htjava} DAFTAT BUY DL ✦-------`,
 	rows: [
-	    {title: "🔖 1 DL", rowId: '.order *Paket:* 1 DL • Sewa', description: 'PRICE: ' + sh + 'Rp ' },
-	    {title: "🔖 2 DL", rowId: '.order *Paket:* 2 DL • Sewa', description: 'PRICE: ' + sn + 'Rp ' },
-	{title: "🔖 3 DL", rowId: '.order *Paket:* 3 DL • Sewa', description: 'PRICE: ' + ss + 'Rp ' },
-	{title: "🔖 4 DL", rowId: '.order *Paket:* 4 DL • Sewa', description: 'PRICE: ' + sp + 'Rp ' },
-	{title: "🔖 5 DL", rowId: '.order *Paket:* 5 DL • Sewa', description: 'PRICE: ' + sv + 'Rp ' },
+	    {title: "🔖 1 DL", rowId: '.order *Paket:* 1 DL • BUY DL', description: 'PRICE: ' + sh + 'Rp' },
+	    {title: "🔖 2 DL", rowId: '.order *Paket:* 2 DL • BUY DL', description: 'PRICE: ' + sn + 'Rp' },
+	{title: "🔖 3 DL", rowId: '.order *Paket:* 3 DL • BUY DL', description: 'PRICE: ' + ss + 'Rp' },
+	{title: "🔖 4 DL", rowId: '.order *Paket:* 4 DL • BUY DL', description: 'PRICE: ' + sp + 'Rp' },
+	{title: "🔖 5 DL", rowId: '.order *Paket:* 5 DL • BUY DL', description: 'PRICE: ' + sv + 'Rp' },
 	]
     }, {
-    title: `${htjava} HARGA SELL DL ✦-------`,
+    title: `${htjava} DAFTAR SELL DL ✦-------`,
 	rows: [
-	    {title: "🌟 1 DL", rowId: '.order *Paket:* 1 DL • Premium', description: 'PRICE: ' + ph + 'Rp ' },
-	    {title: "🌟 2 DL", rowId: '.order *Paket:* 2 DL • Premium', description: 'PRICE: ' + pn + 'Rp ' },
-	{title: "🌟 3 DL", rowId: '.order *Paket:* 3 DL • Premium', description: 'PRICE: ' + pp + 'Rp ' },
-	{title: "🌟 4 DL", rowId: '.order *Paket:* 4 DL • Premium', description: 'PRICE: ' + pv + 'Rp ' },
-	{title: "🌟 5 DL", rowId: '.order *Paket:* 5 DL • Premium', description: 'PRICE: ' + ppm + 'Rp ' },
+	    {title: "🌟 1 DL", rowId: '.order *Paket:* 1 DL • SELL DL', description: 'PRICE: ' + ph + 'Rp' },
+	    {title: "🌟 2 DL", rowId: '.order *Paket:* 2 DL • SELL DL', description: 'PRICE: ' + pn + 'Rp' },
+	{title: "🌟 3 DL", rowId: '.order *Paket:* 3 DL • SELL DL', description: 'PRICE: ' + pp + 'Rp' },
+	{title: "🌟 4 DL", rowId: '.order *Paket:* 4 DL • SELL DL', description: 'PRICE: ' + pv + 'Rp' },
+	{title: "🌟 5 DL", rowId: '.order *Paket:* 5 DL • SELL DL', description: 'PRICE: ' + ppm + 'Rp' },
 	]
     },
 ]
@@ -67,15 +66,15 @@ const listMessage = {
   text: info,
   footer: botdate,
   title: wm,
-  buttonText: "Daftar Harga!",
+  buttonText: "Click Here!",
   sections
 }
 await conn.sendMessage(m.chat, listMessage)
 //conn.sendHydrated(m.chat, info, wm, null, sgc, "🌎 Group Official", null,null, [['Owner','.owner']], m)
 }
 
-handler.help = ['buydl', 'selldl']
-handler.tags = ['info']
-handler.command = /^(buydl)?|gt)$/i
+handler.help = ['sewa', 'premium']
+handler.tags = ['main']
+handler.command = /^(buydl|selldl|gt)$/i
 
 export default handler
